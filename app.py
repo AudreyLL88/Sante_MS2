6db457d93da73040bac40f594f496857b24a4def
AudreyLL88 <a.lloancy@gmail.com>
2020-11-26 10:53:34 +0000

    Update task into the database

--- app.py
+++ app.py
@@ -111,6 +111,19 @@ def add_cocktail():
 
 @app.route("/edit_cocktail/<cocktail_id>", methods=["GET", "POST"])
 def edit_cocktail(cocktail_id):
+    if request.method == "POST":
+        cocktail_status = "on" if request.form.get("cocktail_status") else "off"
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "cocktail_name": request.form.get("cocktail_name"),
+            "cocktail_description": request.form.get("cocktail_description"),
+            "cocktail_status": cocktail_status,
+            "cocktail_date": request.form.get("cocktail_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.cocktails.update({"_id": ObjectId(cocktail_id)}, submit)
+        flash("Cocktail Successfully Updated")
+
     cocktail = mongo.db.cocktails.find_one({"_id": ObjectId(cocktail_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template(
